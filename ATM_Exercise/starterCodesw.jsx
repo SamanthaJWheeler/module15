185e1b0097d5aa9c341dcc0a06096dc4d435ac2d
SamanthaJWheeler <115271437+SamanthaJWheeler@users.noreply.github.com>
2023-02-25 15:33:02 -0500

    deposit and cash back buttons

--- ATM_Exercise/starterCodesw.jsx
+++ ATM_Exercise/starterCodesw.jsx
@@ -1,31 +1,37 @@
 
-const ATMDeposit = ({onChange}) => {
+const ATMDeposit = ({onChange, isDeposit}) => {
+  const choice = ["Deposit", "Cash Back"];
   return (
     <label className="label huge">
-      Deposit:
-      <input type="number" onChange={onChange}></input>
-      <input type="submit" value = "Submit"></input>
+      <h3> {choice[Number(!isDeposit)]}</h3>
+      <input type="number" width="400" onChange={onChange}></input>
+      <input type="submit" width="400" value = "Submit"></input>
     </label>
   );
 };
 
 const Account = () => {
-    let transactionState = 0; // state of this transaction
+    let deposit = 0; // state of this transaction
     const [totalState, setTotalState] = React.useState(0);
+    const [isDeposit, setIsDeposit] = React.useState(true);
+    
     let status = `Account Balance $ ${totalState}`;
-    console.log(`Account Rendered`);
+    //console.log(`Account Rendered`);
     const handleChange = event => {
       console.log(`handleChange ${event.target.value}`);
-      transactionState = Number(event.target.value);
+      deposit = Number(event.target.value);
   };
     const handleSubmit = () => {
-      setTotalState(totalState + transactionState);
+      let newTotal = isDeposit ? totalState + deposit: totalState - deposit;
+      setTotalState(newTotal);
       event.preventDefault();
   };
   return (
     <form onSubmit={handleSubmit}>
       <h2 id = "total">{status}</h2>
-      <ATMDeposit onChange={handleChange}> Deposit</ATMDeposit>
+      <button onClick={() => setIsDeposit(true)}>Deposit</button>
+      <button onClick={() => setIsDeposit(false)}>Cash Back</button>
+      <ATMDeposit onChange={handleChange} isDeposit={isDeposit}></ATMDeposit>
     </form>
   );
 };
