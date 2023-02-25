40eb131d92aea09d107e0e478101f8156351db21
SamanthaJWheeler <115271437+SamanthaJWheeler@users.noreply.github.com>
2023-02-25 14:37:26 -0500

    edits

--- ATM_Exercise/solutionStage1.jsx
+++ ATM_Exercise/solutionStage1.jsx
@@ -3,24 +3,28 @@ const ATMDeposit = ({ onChange }) => {
     <label className="label huge">
       Deposit:
       <input type="number" onChange={onChange}></input>
-      <input type="submit"></input>
+      <input type="submit" value="Submit"></input>
     </label>
   );
 };
 
 const Account = () => {
- const [accountState, setAccountState] = React.useState(0); 
- const handleChange = event => {
+  let transactionState = 0; // state of this transaction
+  let totalState = 0; // Account total at Bank
+  let status = "Account Balance $zero";
+  const handleChange = event => {
     console.log(`handleChange ${event.target.value}`);
-    setAccountState = Number(event.target.value);
+    transactionState = Number(event.target.value);
   };
   const handleSubmit = () => {
-    alert(`Account total= ${accountState}`);
-    Event.preventDefault();
+    totalState += transactionState;
+    status = `Account Balance $ ${totalState}`;
+    document.getElementById("total").innerHTML = status;
+    event.preventDefault();
   };
   return (
     <form onSubmit={handleSubmit}>
-      <h2>Account Balance {accountState}</h2>
+      <h2 id="total">{status}</h2>
       <ATMDeposit onChange={handleChange}> Deposit</ATMDeposit>
     </form>
   );
